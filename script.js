f9c8f76bce01bd1a38f0d9b272ad04545a0a745d
Isha Kaushik <185116282+IK040903@users.noreply.github.com>
2025-10-27 17:46:16 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+// script.js
+document.getElementById('year').textContent = new Date().getFullYear();
+
+function scrollToEl(id){
+  const el = document.getElementById(id);
+  if(el){
+    el.scrollIntoView({ behavior: 'smooth', block: 'start' });
+  }
+}
 
